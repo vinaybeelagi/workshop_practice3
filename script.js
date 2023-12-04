13861407d764e7c904d1f8bfa7cca160a0349693
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-04 10:44:09 +0530

    updated table space and footer

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-// function open(){
-//   document.getElementById("toggleButton").style.width = "300px"
-// }
-// function close(){
-//   document.getElementById("toggleButton").style.width = "160px"
-// }
+function toggleSidebar(initialSetup = false) {
+  const sidebar = document.getElementById('sidebar');
+  const main = document.getElementById('main');
+  const button = document.getElementById('sidebarCollapse');
+  const sidebarImage = document.getElementById('sidebarImage');
+  const icon1 = document.getElementById('tab_one_name');
+  const icon2 = document.getElementById('tab_two_name');
+  const greenDiv = document.querySelector('.green-div');
 
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const main = document.getElementById('main');
-    const button = document.getElementById('sidebarCollapse');
-    const sidebarImage = document.getElementById('sidebarImage');
-    const icon1  = document.getElementById("tab_one_name")
-    const icon2 = document.getElementById("tab_two_name")
-  
-    if (main.classList.contains('shifted')) {
-      sidebar.style.width = '160px';
-      main.classList.remove('shifted');
-      button.textContent = '>';
-      sidebarImage.style.width = "78px";
-      sidebarImage.style.height = "36px";
-      icon1.style.display = "none"
-      icon2.style.display = "none"
-      sidebarCollapse.style.left = '155px'
-      sidebarCollapse.style.top = '48px'
-      sidebarImage.src ="/assets/Group 1.svg";
+  const updateStyles = (width, iconDisplay, imageSrc, collapseLeft, greenDivDisplay) => {
+    sidebar.style.width = width;
+    icon1.style.display = icon2.style.display = iconDisplay;
+    sidebarCollapse.style.left = collapseLeft;
+    greenDiv.style.display = greenDivDisplay;
+
+    // Set resolution for sidebarImage when the width is 300px
+    if (width === '300px') {
+      sidebarImage.style.width = '240px';
+      sidebarImage.style.height = '80px';
     } else {
-      sidebar.style.width = '300px';
-      main.classList.add('shifted');
-      sidebarImage.style.width = "240px"
-      sidebarImage.style.height = "80px"
-      icon2.style.display = ""
-      icon1.style.display = ""
-      sidebarCollapse.style.left = '270px'
-      sidebarCollapse.style.top = '48px'
-      button.textContent = '<';
-      sidebarImage.src = './assets/aftermain_logo.svg';
+      sidebarImage.style.width = '78px';
+      sidebarImage.style.height = '36px';
     }
+
+    sidebarImage.src = imageSrc;
+  };
+
+  if (initialSetup) {
+    updateStyles('160px', 'none', '/assets/Group 1.svg', '155px', 'none');
+    main.classList.remove('shifted');
+    button.innerHTML = '<img src="/assets/bxs-chevron-left.svg" alt="Right Arrow" class="ml-3 rotate-right">';
+    return;
   }
-  
-  
-    
+
+  const isShifted = main.classList.contains('shifted');
+  updateStyles(
+    isShifted ? '160px' : '300px',
+    isShifted ? 'none' : '',
+    isShifted ? '/assets/Group 1.svg' : './assets/aftermain_logo.svg',
+    isShifted ? '155px' : '270px',
+    isShifted ? 'none' : 'block'
+  );
+  main.classList.toggle('shifted');
+  button.innerHTML = `<img src="/assets/bxs-chevron-left.svg" alt="Right Arrow" class="ml-3 rotate-${isShifted ? 'right' : 'left'}">`;
+}
+
+document.addEventListener('DOMContentLoaded', () => toggleSidebar(true));
